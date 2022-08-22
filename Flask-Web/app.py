65691f2c4d5c9f9f10c6e8dc7603b59828e3be32
yunhacho <jyh_5530@naver.com>
2022-08-22 14:27:13 +0900

    flask app.py

---
+++ Flask-Web/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from elasticsearch import Elasticsearch
+import json
+
+from SearchAPI.PrefixSearch import PrefixSearch
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('searchHome.html')
+
+@app.route('/search', methods=['GET'])
+def search() :
+    host = '0.0.0.0'; port = '9200'
+    index = 'entire_krx_tckr'
+    keyword = request.args.get('keyword')
+    result = PrefixSearch(host, port, index).search(keyword)
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run()
