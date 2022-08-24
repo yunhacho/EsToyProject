f3732bb52327025c29782c3a79356145b978c2d5
yunhacho <jyh_5530@naver.com>
2022-08-24 20:04:55 +0900

    app.py

--- Flask-Web/app.py
+++ Flask-Web/app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from elasticsearch import Elasticsearch
 import json
 
 from SearchAPI.PrefixSearch import PrefixSearch
 
-
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,9 +17,7 @@ def search() :
     index = 'entire_krx_tckr'
     keyword = request.args.get('keyword')
     result = PrefixSearch(host, port, index).search(keyword)
-    print(json.dumps(result, ensure_ascii=False, indent=2))
     return json.dumps(result)
-
 
 if __name__ == '__main__':
     app.run()
